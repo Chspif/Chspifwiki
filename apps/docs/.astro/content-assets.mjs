b8172797a1d1fc6c0dd722a495f05df5d7645dbb
1542761533 <1542761533@qq.com>
2025-12-07 02:39:28 +0800

    123

---
+++ apps/docs/.astro/content-assets.mjs
@@ -0,0 +1,4 @@
+
+import __ASTRO_IMAGE_IMPORT_22Azrb from "../../assets/catppuccin.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
+export default new Map([["../../assets/catppuccin.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_22Azrb]]);
+		
